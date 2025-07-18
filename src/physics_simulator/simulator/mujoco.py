ef3de862bde30320e89cfc899819fdb872390470
ruziniuuuuu <ruziniuuuuu@gmail.com>
2025-07-18 21:56:25 +0800

    let num_steps = 20 in loop()

--- src/physics_simulator/simulator/mujoco.py
+++ src/physics_simulator/simulator/mujoco.py
@@ -350,7 +350,7 @@ class MujocoSimulator(BaseSim):
             num_steps = int(elapsed / self.config.mujoco_config.timestep)
 
             # self.step(num_steps)
-            self.step(num_steps=1)
+            self.step(num_steps=20)
 
             if self._physics_callbacks:
                 callbacks = list(self._physics_callbacks.values())
