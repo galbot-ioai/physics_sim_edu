8fcdbf6e26bed3354956af6bafe997dcd3e2ded7
ruziniuuuuu <ruziniuuuuu@gmail.com>
2025-07-23 11:10:30 +0800

    align depth camera to rgb camera for realsense d436

--- src/synthnova_config/src/synthnova_config/sensor/realsense_d436.py
+++ src/synthnova_config/src/synthnova_config/sensor/realsense_d436.py
@@ -69,31 +69,31 @@ class RealsenseD436DepthSensorConfig(DepthSensorConfig):
     """
     Configuration class for the RealSense D436 Depth camera.
 
-    Intrinsics of "Depth" / 1280x720 / {Z16}:
+    Intrinsics of "Color" / 1280x720 / {YUYV/RGB8/BGR8/RGBA8/BGRA8/Y8}:
       - Width:         1280
       - Height:        720
-      - PPX:          643.402221679688
-      - PPY:          355.124542236328
-      - Fx:           641.102600097656
-      - Fy:           641.102600097656
-      - Distortion:   Brown Conrady (using plumb_bob model)
+      - PPX:          636.495544433594
+      - PPY:          363.409881591797
+      - Fx:           638.315795898438
+      - Fy:           637.68310546875
+      - Distortion:   Inverse Brown Conrady (using plumb_bob model)
       - Coeffs:       [0.0, 0.0, 0.0, 0.0, 0.0]
-      - FOV (deg):    89.9 x 58.63
+      - FOV (deg):    90.15 x 58.89
     """
 
-    frequency: int = 30
-    width: int = 1280
-    height: int = 720
-    fx: float = 641.102600097656
-    fy: float = 641.102600097656
-    cx: float = 643.402221679688
-    cy: float = 355.124542236328
-    pixel_size: float = 0.003
-    f_stop: float = 0.0
-    focus_distance: float = 0.0
-    projection_type: Literal["pinhole"] = "pinhole"
-    clipping_range: List[float] = [0.05, 15.0]
-    distortion_model: Literal["plumb_bob"] = "plumb_bob"
-    distortion_coefficients: List[float] = [0.0, 0.0, 0.0, 0.0, 0.0]
-    horizontal_fov: float = 89.9
-    vertical_fov: float = 58.63
+    frequency: float | None = 30.0
+    width: int | None = 1280
+    height: int | None = 720
+    fx: float | None = 638.315795898438
+    fy: float | None = 637.68310546875
+    cx: float | None = 636.495544433594
+    cy: float | None = 363.409881591797
+    pixel_size: float | None = 0.003
+    f_stop: float | None = 0.0
+    focus_distance: float | None = 0.0
+    projection_type: str | None = "pinhole"
+    clipping_range: list[float] | None = [0.05, 15.0]
+    distortion_model: str | None = "plumb_bob"
+    distortion_coefficients: list[float] | None = [0.0, 0.0, 0.0, 0.0, 0.0]
+    horizontal_fov: float | None = 90.15
+    vertical_fov: float | None = 58.89
