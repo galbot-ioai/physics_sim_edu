f28b45c51b01ca9a4c74de1b5a043967877ea6f4
ruziniuuuuu <ruziniuuuuu@gmail.com>
2025-07-30 19:13:19 +0800

    update grasp env

--- examples/ioai_examples/ioai_grasp_env.py
+++ examples/ioai_examples/ioai_grasp_env.py
@@ -305,7 +305,7 @@ class IoaiGraspEnv:
         # Add cube
         cube_config = CuboidConfig(
             prim_path="/World/Cube",
-            position=[0.65, 0.2, 0.56],
+            position=[0.6, 0.2, 0.56],
             orientation=[0, 0, 0, 1],
             scale=[0.05, 0.05, 0.05],
             color=[0.5, 0.5, 0.5],  # Gray color
